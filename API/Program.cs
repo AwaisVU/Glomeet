e4f3dd3bda1ea4aa439fe3bcc8bbad690f52af35
AwaisVU <eng.awaisch@gmail.com>
2026-06-26 22:21:39 +0100

    End of dev phase 2

---
+++ API/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Add Middleware
+
+app.MapControllers();
+
+//Define Scope of Services and seed the data
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedData(context);
+}
+catch(Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An Error occurred during Migration");
+}
+
+app.Run();
